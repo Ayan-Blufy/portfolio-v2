2b7642a8f06038b98cd636d295cbaef0e3ba2e96
Ayan <ayan@emertech.io>
2025-10-12 12:28:35 +0530

    add: highlighting of tech stack

--- components/portfolio.tsx
+++ components/portfolio.tsx
@@ -197,8 +197,16 @@ export default function Portfolio() {
                   Where I contribute to the design and scale APIs and
                   microservices that power our products and integrations, with a
                   focus on performance, reliability, security, and clean data
-                  models (Node.js/TypeScript, Prisma, PostgreSQL, Redis,
-                  AWS,Circle ci, Docker,MongoDB,Nest js,etc).
+                  models ({" "}
+                  <span className="text-teal-300"> Node Js/TypeScript </span>,
+                  <span className="text-teal-300"> Prisma </span>,
+                  <span className="text-teal-300"> Nest Js </span>,
+                  <span className="text-teal-300"> PostgreSQL </span>,
+                  <span className="text-teal-300"> Redis </span>,
+                  <span className="text-teal-300"> AWS </span>,
+                  <span className="text-teal-300"> Circle ci </span>,
+                  <span className="text-teal-300"> Docker </span>,
+                  <span className="text-teal-300"> MongoDB </span> ,etc).
                 </p>
                 <p className="mb-4">
                   I am also working on a use case called{" "}
@@ -449,8 +457,6 @@ export default function Portfolio() {
                     </div>
                   </li>
 
-
-
                   <li className="mb-12">
                     <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
                       <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
@@ -489,8 +495,6 @@ export default function Portfolio() {
                       />
                     </div>
                   </li>
-
-                
                 </ol>
               </div>
             </section>
